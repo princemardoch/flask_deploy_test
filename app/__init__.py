7691ecc391ef63c4ce79df6d31b6287407e4bde0
leprincemardoch <doublepo56@gmail.com>
2024-09-23 23:47:15 +0000

    Add other config && Session bug correction

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,16 @@ from flask import Flask, session
 
 app = Flask(__name__)
 
+app.url_map.strict_slashes = False
+
 app.config['SECRET_KEY'] = secrets.token_urlsafe(30)
-session.permanent = True
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
 
 from .user.userr import user
 from .auth.authr import auth
